633418c31b889daf277d9cbe011e849b24198687
AKWLD <gmrtkf101@gmail.com>
2025-11-24 17:02:00 +0900

    구구단 써넣음

--- UnityStudy_Origin/Program.cs
+++ UnityStudy_Origin/Program.cs
@@ -4,13 +4,24 @@
     {
         static void Main(string[] args)
         {
-            int num = 1;
+            //int num = 1;
 
-            while (num <= 10)
+            //while (num <= 10)
+            //{
+            //    Console.WriteLine(num);
+
+            //    num++;
+
             {
-                Console.WriteLine(num);
+                for (int z = 2; z <= 9; z++)
+                {
+                    Console.WriteLine($"{z}단");  
 
-                num++;
+                    for (int x = 1; x <= 9; x++)
+                       
+                        Console.WriteLine($" {z}X{x} = {x*z}");
+                    Console.WriteLine();
+                }
             }
         }
     }
